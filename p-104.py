e83b284cd7dc3314588953cca606d23fbcc4413e
Aditi <74664973+aditineemkar@users.noreply.github.com>
2021-11-11 11:36:32 +0530

    Update p-104.py

--- p-104.py
+++ p-104.py
@@ -36,15 +36,41 @@ print("Median :-"+str(median))
 
 #mode 
 data = Counter(new_data)
-modeRange = {"50-60":0,"60-70":0,"70-80":0}
+modeRange = {
+    "75-85":0,
+    "85-95":0,
+    "95-105":0,
+    "105-115":0,
+    "115-125":0,
+    "125-135":0,
+    "135-145":0,
+    "145-155":0,
+    "155-165":0,
+    "165-175":0,
+}
 
 for height,occurence in data.items():
-    if 50<float(height)<60:
-        modeRange["50-60"]= modeRange["50-60"] + occurence
-    elif 60<float(height)<70:
-        modeRange["60-70"] = modeRange["60-70"] + occurence
-    elif 70<float(height)<80:
-        modeRange["70-80"] = modeRange["70-80"] + occurence
+    if 75<float(height)<85:
+        modeRange["75-85"]= + occurence
+    elif 85<float(height)<95:
+        modeRange["85-95"] = + occurence
+    elif 95<float(height)<105:
+        modeRange["95-105"] = + occurence
+    elif 105<float(height)<115:
+        modeRange["105-115"] = + occurence
+    elif 115<float(height)<125:
+        modeRange["115-125"] = + occurence
+    elif 125<float(height)<135:
+        modeRange["125-135"] = + occurence
+    elif 135<float(height)<145:
+        modeRange["135-145"] = + occurence
+    elif 145<float(height)<155:
+        modeRange["145-155"] = + occurence
+    elif 155<float(height)<165:
+        modeRange["155-165"] = + occurence
+    elif 165<float(height)<175:
+        modeRange["165-175"] = + occurence
+
 
 mode_range, mode_occurence = 0,0
 for range, occurence in modeRange.items():
